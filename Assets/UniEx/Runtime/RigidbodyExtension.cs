26b1628f676003ede6ad27f7161fe0642882b1a0
kitatas <315kitatas10703@gmail.com>
2023-01-09 20:49:34 +0900

    add: RigidbodyExtension

---
+++ Assets/UniEx/Runtime/RigidbodyExtension.cs
@@ -0,0 +1,319 @@
+using UnityEngine;
+
+namespace UniEx
+{
+    public static class RigidbodyExtension
+    {
+        #region angularVelocity
+
+        /// <summary>
+        /// Set the angularVelocity x of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="x">Set value.</param>
+        /// <returns>Set result.</returns>
+        public static Rigidbody SetAngularVelocityX(this Rigidbody self, float x)
+        {
+            var velocity = self.angularVelocity;
+            self.angularVelocity = velocity.SetX(x);
+            return self;
+        }
+
+        /// <summary>
+        /// Set the angularVelocity y of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="y">Set value.</param>
+        /// <returns>Set result.</returns>
+        public static Rigidbody SetAngularVelocityY(this Rigidbody self, float y)
+        {
+            var velocity = self.angularVelocity;
+            self.angularVelocity = velocity.SetY(y);
+            return self;
+        }
+
+        /// <summary>
+        /// Set the angularVelocity z of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="z">Set value.</param>
+        /// <returns>Set result.</returns>
+        public static Rigidbody SetAngularVelocityZ(this Rigidbody self, float z)
+        {
+            var velocity = self.angularVelocity;
+            self.angularVelocity = velocity.SetZ(z);
+            return self;
+        }
+
+        /// <summary>
+        /// Reset the angularVelocity of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <returns>Reset result.</returns>
+        public static Rigidbody ResetAngularVelocity(this Rigidbody self)
+        {
+            self.angularVelocity = Vector3.zero;
+            return self;
+        }
+
+        /// <summary>
+        /// Add the angularVelocity x of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="x">Add value.</param>
+        /// <returns>Add result.</returns>
+        public static Rigidbody AddAngularVelocityX(this Rigidbody self, float x)
+        {
+            return self.SetAngularVelocityX(self.angularVelocity.x + x);
+        }
+
+        /// <summary>
+        /// Add the angularVelocity y of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="y">Add value.</param>
+        /// <returns>Add result.</returns>
+        public static Rigidbody AddAngularVelocityY(this Rigidbody self, float y)
+        {
+            return self.SetAngularVelocityY(self.angularVelocity.y + y);
+        }
+
+        /// <summary>
+        /// Add the angularVelocity z of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="z">Add value.</param>
+        /// <returns>Add result.</returns>
+        public static Rigidbody AddAngularVelocityZ(this Rigidbody self, float z)
+        {
+            return self.SetAngularVelocityZ(self.angularVelocity.z + z);
+        }
+
+        /// <summary>
+        /// Multiply the angularVelocity x of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="x">Multiply value.</param>
+        /// <returns>Multiply result.</returns>
+        public static Rigidbody MultiplyAngularVelocityX(this Rigidbody self, float x)
+        {
+            return self.SetAngularVelocityX(self.angularVelocity.x * x);
+        }
+
+        /// <summary>
+        /// Multiply the angularVelocity y of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="y">Multiply value.</param>
+        /// <returns>Multiply result.</returns>
+        public static Rigidbody MultiplyAngularVelocityY(this Rigidbody self, float y)
+        {
+            return self.SetAngularVelocityY(self.angularVelocity.y * y);
+        }
+
+        /// <summary>
+        /// Multiply the angularVelocity z of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="z">Multiply value.</param>
+        /// <returns>Multiply result.</returns>
+        public static Rigidbody MultiplyAngularVelocityZ(this Rigidbody self, float z)
+        {
+            return self.SetAngularVelocityZ(self.angularVelocity.z * z);
+        }
+
+        /// <summary>
+        /// Clamps the angularVelocity x between the given minimum float and maximum float values. Returns the given value if it is within the minimum and maximum range.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="min">The minimum floating point value to compare against.</param>
+        /// <param name="max">The maximum floating point value to compare against.</param>
+        /// <returns>Clamp result.</returns>
+        public static Rigidbody ClampAngularVelocityX(this Rigidbody self, float min, float max)
+        {
+            return self.SetAngularVelocityX(Mathf.Clamp(self.angularVelocity.x, min, max));
+        }
+
+        /// <summary>
+        /// Clamps the angularVelocity y between the given minimum float and maximum float values. Returns the given value if it is within the minimum and maximum range.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="min">The minimum floating point value to compare against.</param>
+        /// <param name="max">The maximum floating point value to compare against.</param>
+        /// <returns>Clamp result.</returns>
+        public static Rigidbody ClampAngularVelocityY(this Rigidbody self, float min, float max)
+        {
+            return self.SetAngularVelocityY(Mathf.Clamp(self.angularVelocity.y, min, max));
+        }
+
+        /// <summary>
+        /// Clamps the angularVelocity z between the given minimum float and maximum float values. Returns the given value if it is within the minimum and maximum range.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="min">The minimum floating point value to compare against.</param>
+        /// <param name="max">The maximum floating point value to compare against.</param>
+        /// <returns>Clamp result.</returns>
+        public static Rigidbody ClampAngularVelocityZ(this Rigidbody self, float min, float max)
+        {
+            return self.SetAngularVelocityZ(Mathf.Clamp(self.angularVelocity.z, min, max));
+        }
+
+        #endregion
+
+        #region velocity
+
+        /// <summary>
+        /// Set the velocity x of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="x">Set value.</param>
+        /// <returns>Set result.</returns>
+        public static Rigidbody SetVelocityX(this Rigidbody self, float x)
+        {
+            var velocity = self.velocity;
+            self.velocity = velocity.SetX(x);
+            return self;
+        }
+
+        /// <summary>
+        /// Set the velocity y of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="y">Set value.</param>
+        /// <returns>Set result.</returns>
+        public static Rigidbody SetVelocityY(this Rigidbody self, float y)
+        {
+            var velocity = self.velocity;
+            self.velocity = velocity.SetY(y);
+            return self;
+        }
+
+        /// <summary>
+        /// Set the velocity z of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="z">Set value.</param>
+        /// <returns>Set result.</returns>
+        public static Rigidbody SetVelocityZ(this Rigidbody self, float z)
+        {
+            var velocity = self.velocity;
+            self.velocity = velocity.SetZ(z);
+            return self;
+        }
+
+        /// <summary>
+        /// Reset the velocity of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <returns>Reset result.</returns>
+        public static Rigidbody ResetVelocity(this Rigidbody self)
+        {
+            self.velocity = Vector3.zero;
+            return self;
+        }
+
+        /// <summary>
+        /// Add the velocity x of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="x">Add value.</param>
+        /// <returns>Add result.</returns>
+        public static Rigidbody AddVelocityX(this Rigidbody self, float x)
+        {
+            return self.SetVelocityX(self.velocity.x + x);
+        }
+
+        /// <summary>
+        /// Add the velocity y of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="y">Add value.</param>
+        /// <returns>Add result.</returns>
+        public static Rigidbody AddVelocityY(this Rigidbody self, float y)
+        {
+            return self.SetVelocityY(self.velocity.y + y);
+        }
+
+        /// <summary>
+        /// Add the velocity z of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="z">Add value.</param>
+        /// <returns>Add result.</returns>
+        public static Rigidbody AddVelocityZ(this Rigidbody self, float z)
+        {
+            return self.SetVelocityZ(self.velocity.z + z);
+        }
+
+        /// <summary>
+        /// Multiply the velocity x of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="x">Multiply value.</param>
+        /// <returns>Multiply result.</returns>
+        public static Rigidbody MultiplyVelocityX(this Rigidbody self, float x)
+        {
+            return self.SetVelocityX(self.velocity.x * x);
+        }
+
+        /// <summary>
+        /// Multiply the velocity y of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="y">Multiply value.</param>
+        /// <returns>Multiply result.</returns>
+        public static Rigidbody MultiplyVelocityY(this Rigidbody self, float y)
+        {
+            return self.SetVelocityY(self.velocity.y * y);
+        }
+
+        /// <summary>
+        /// Multiply the velocity z of the Rigidbody.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="z">Multiply value.</param>
+        /// <returns>Multiply result.</returns>
+        public static Rigidbody MultiplyVelocityZ(this Rigidbody self, float z)
+        {
+            return self.SetVelocityZ(self.velocity.z * z);
+        }
+
+        /// <summary>
+        /// Clamps the velocity x between the given minimum float and maximum float values. Returns the given value if it is within the minimum and maximum range.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="min">The minimum floating point value to compare against.</param>
+        /// <param name="max">The maximum floating point value to compare against.</param>
+        /// <returns>Clamp result.</returns>
+        public static Rigidbody ClampVelocityX(this Rigidbody self, float min, float max)
+        {
+            return self.SetVelocityX(Mathf.Clamp(self.velocity.x, min, max));
+        }
+
+        /// <summary>
+        /// Clamps the velocity y between the given minimum float and maximum float values. Returns the given value if it is within the minimum and maximum range.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="min">The minimum floating point value to compare against.</param>
+        /// <param name="max">The maximum floating point value to compare against.</param>
+        /// <returns>Clamp result.</returns>
+        public static Rigidbody ClampVelocityY(this Rigidbody self, float min, float max)
+        {
+            return self.SetVelocityY(Mathf.Clamp(self.velocity.y, min, max));
+        }
+
+        /// <summary>
+        /// Clamps the velocity z between the given minimum float and maximum float values. Returns the given value if it is within the minimum and maximum range.
+        /// </summary>
+        /// <param name="self">Self rigidbody.</param>
+        /// <param name="min">The minimum floating point value to compare against.</param>
+        /// <param name="max">The maximum floating point value to compare against.</param>
+        /// <returns>Clamp result.</returns>
+        public static Rigidbody ClampVelocityZ(this Rigidbody self, float min, float max)
+        {
+            return self.SetVelocityZ(Mathf.Clamp(self.velocity.z, min, max));
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/UniEx/Tests/Runtime/RigidbodyTest.cs
@@ -0,0 +1,333 @@
+using NUnit.Framework;
+using UnityEngine;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace UniEx.Tests
+{
+    public sealed class RigidbodyTest
+    {
+        [Test]
+        public void SetAngularVelocity()
+        {
+            // Set AngularVelocity X
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.SetAngularVelocityX(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.x, 0.5f);
+            }
+
+            // Set AngularVelocity Y
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.SetAngularVelocityY(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.y, 0.5f);
+            }
+
+            // Set AngularVelocity Z
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.SetAngularVelocityZ(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.z, 0.5f);
+            }
+
+            // Reset AngularVelocity
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ResetAngularVelocity();
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.x, 0.0f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.y, 0.0f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.z, 0.0f);
+            }
+        }
+
+        [Test]
+        public void AddAngularVelocity()
+        {
+            // Add AngularVelocity X
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.AddAngularVelocityX(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.x, 0.8f);
+            }
+
+            // Add AngularVelocity Y
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.AddAngularVelocityY(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.y, 0.8f);
+            }
+
+            // Add AngularVelocity Z
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.AddAngularVelocityZ(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.z, 0.8f);
+            }
+        }
+
+        [Test]
+        public void MultiplyAngularVelocity()
+        {
+            // Multiply AngularVelocity X
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.MultiplyAngularVelocityX(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.x, 0.15f);
+            }
+
+            // Multiply AngularVelocity Y
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.MultiplyAngularVelocityY(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.y, 0.15f);
+            }
+
+            // Multiply AngularVelocity Z
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.MultiplyAngularVelocityZ(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.z, 0.15f);
+            }
+        }
+
+        [Test]
+        public void ClampAngularVelocity()
+        {
+            // Clamp AngularVelocity X 1
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampAngularVelocityX(0.1f, 0.2f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.x, 0.2f);
+            }
+
+            // Clamp AngularVelocity X 2
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampAngularVelocityX(0.4f, 0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.x, 0.4f);
+            }
+
+            // Clamp AngularVelocity Y 1
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampAngularVelocityY(0.1f, 0.2f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.y, 0.2f);
+            }
+
+            // Clamp AngularVelocity Y 2
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampAngularVelocityY(0.4f, 0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.y, 0.4f);
+            }
+
+            // Clamp AngularVelocity Z
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampAngularVelocityZ(0.1f, 0.2f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.z, 0.2f);
+            }
+
+            // Clamp AngularVelocity Z
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.angularVelocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampAngularVelocityZ(0.4f, 0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.angularVelocity.z, 0.4f);
+            }
+        }
+
+        [Test]
+        public void SetVelocity()
+        {
+            // Set Velocity X
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.SetVelocityX(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.x, 0.5f);
+            }
+
+            // Set Velocity Y
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.SetVelocityY(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.y, 0.5f);
+            }
+
+            // Set Velocity Z
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.SetVelocityZ(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.z, 0.5f);
+            }
+
+            // Reset AngularVelocity
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ResetVelocity();
+                Assert.AreApproximatelyEqual(rigidbody.velocity.x, 0.0f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.y, 0.0f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.z, 0.0f);
+            }
+        }
+
+        [Test]
+        public void AddVelocity()
+        {
+            // Add Velocity X
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.AddVelocityX(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.x, 0.8f);
+            }
+
+            // Add Velocity Y
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.AddVelocityY(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.y, 0.8f);
+            }
+
+            // Add Velocity Z
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.AddVelocityZ(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.z, 0.8f);
+            }
+        }
+
+        [Test]
+        public void MultiplyVelocity()
+        {
+            // Multiply Velocity X
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.MultiplyVelocityX(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.x, 0.15f);
+            }
+
+            // Multiply Velocity Y
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.MultiplyVelocityY(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.y, 0.15f);
+            }
+
+            // Multiply Velocity Z
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.MultiplyVelocityZ(0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.z, 0.15f);
+            }
+        }
+
+        [Test]
+        public void ClampVelocity()
+        {
+            // Clamp Velocity X 1
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampVelocityX(0.1f, 0.2f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.x, 0.2f);
+            }
+
+            // Clamp Velocity X 2
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampVelocityX(0.4f, 0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.x, 0.4f);
+            }
+
+            // Clamp Velocity Y 1
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampVelocityY(0.1f, 0.2f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.y, 0.2f);
+            }
+
+            // Clamp Velocity Y 2
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampVelocityY(0.4f, 0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.y, 0.4f);
+            }
+
+            // Clamp Velocity Z 1
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampVelocityZ(0.1f, 0.2f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.z, 0.2f);
+            }
+
+            // Clamp Velocity Z 2
+            {
+                var gameObject = new GameObject();
+                var rigidbody = gameObject.AddComponent<Rigidbody>();
+                rigidbody.velocity = new Vector3(0.3f, 0.3f, 0.3f);
+                rigidbody.ClampVelocityZ(0.4f, 0.5f);
+                Assert.AreApproximatelyEqual(rigidbody.velocity.z, 0.4f);
+            }
+        }
+    }
+}
